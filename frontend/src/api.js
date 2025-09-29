4d1d1085142e94bead3439607b9192d0a805edc6
yashintodia7 <132248506+yashintodia7@users.noreply.github.com>
2025-09-29 09:42:12 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
-  withCredentials: true, // important to send/receive cookies
+  withCredentials: true, 
 });
 
 export default api;
